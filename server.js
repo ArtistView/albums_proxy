8c17f996949e3fbe6618b7b009b1748abec78966
mjrosenberg <mjr70@duke.edu>
2020-06-17 17:24:33 -0700

    added a comment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const PORT = 8000;
+const PORT = 8000; // running my proxy on port 8000
 const app = express();
 
 app.use(bodyParser());
