715c3e4db06fe2077c59e81012f4d090038ababf
mjrosenberg <mjr70@duke.edu>
2020-06-17 17:18:09 -0700

    set up basic working server for proxy

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const PORT = 8000;
+const app = express();
+
+app.use(bodyParser());
+app.use(cors());
+app.use(express.static('client'));
+// statically load the index.html file
+
+// write get requests to pull in the various features from the outer FEC folder
+
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+});
