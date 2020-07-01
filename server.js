2960a80a4baccc1240e26eaa38dc518165e3e878
mjrosenberg <mjr70@duke.edu>
2020-07-01 12:01:48 -0700

    proxy server nearly working

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const Promise = require('bluebird');
 
 const PORT = 8000; // running my proxy on port 8000
 const app = express();
 
+app.use(express.static('client'));
 app.use(bodyParser());
 app.use(cors());
-app.use(express.static('client'));
 // statically load the index.html file
-
+const status = app.get('/', (req, res) => {
+  console.log('status');
+  res.status(200).send('connected');
+  res.end();
+});
 // write get requests to pull in the various features from the outer FEC folder
 
 app.listen(PORT, () => {
